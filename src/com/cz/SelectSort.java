758af1dfb5ffc32c37febf8d4b5c0c3e7ec481c7
asuka9910 <573095222@qq.com>
2020-07-03 18:34:46 +0800

    update sort

---
+++ src/com/cz/SelectSort.java
@@ -0,0 +1,30 @@
+package com.cz;
+import java.util.Arrays;
+
+public class SelectSort {
+
+        public static void main(String[] args) {
+            int [] arr = {49,38,65,97,76,13,27,49};
+            selectSort(arr,arr.length);
+        }
+
+        public static void selectSort(int [] arr,int n){
+            for (int i = 0; i < n - 1; i++) {
+                int index = i;
+                int j;
+                // 找出最小值得元素下标
+                for (j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[index]) {
+                        index = j;
+                    }
+                }
+                int tmp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = tmp;
+                System.out.println(Arrays.toString(arr));
+            }
+
+        }
+    }
+
+
